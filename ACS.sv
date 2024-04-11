4102ae165f4e567dfeae1de294c25eab885c33b4
andytu101 <160212545+andytu101@users.noreply.github.com>
2024-04-10 20:51:43 -0700

    Add files via upload

---
+++ ACS.sv
@@ -0,0 +1,58 @@
+module ACS		                        // add-compare-select
+(
+   input       path_0_valid,
+   input       path_1_valid,
+   input [1:0] path_0_bmc,	            // branch metric computation
+   input [1:0] path_1_bmc,				
+   input [7:0] path_0_pmc,				// path metric computation
+   input [7:0] path_1_pmc,
+
+   output logic        selection,
+   output logic        valid_o,
+   output      [7:0] path_cost);  
+
+   logic [7:0] path_cost_0;			   // branch metric + path metric
+   logic  [7:0] path_cost_1; 
+
+	
+	assign path_cost = (valid_o?(selection?path_cost_1:path_cost_0):8'd0);
+	assign path_cost_0 = path_0_bmc + path_0_pmc;
+	assign path_cost_1 = path_1_bmc + path_1_pmc;
+	
+	always_comb begin
+	valid_o = (!path_0_valid && !path_1_valid) ? 1'b0 : 1'b1;
+	 
+		if(path_0_valid == 0 && path_1_valid == 0) begin
+			selection = 0;
+			valid_o = 0;
+			
+		end
+
+		else if(path_0_valid == 0 && path_1_valid == 1) begin
+			selection = 1;
+			valid_o = 1;
+			
+		end
+		
+		else if(path_0_valid == 1 && path_1_valid == 0) begin
+			selection = 0;
+		   valid_o = 1;	
+			
+		end
+		
+		else if(path_cost_0 > path_cost_1) begin
+				selection = 1;
+				valid_o = 1;
+				
+		end
+		
+		else begin
+				selection = 0;
+				valid_o = 1;
+				
+		end
+		
+		
+	end
+			
+endmodule
---
+++ viterbi_tx_rx(a7).sv
@@ -0,0 +1,74 @@
+module viterbi_tx_rx(
+   input    clk,
+   input    rst,
+   input    encoder_i,
+   input    enable_encoder_i,
+   output   decoder_o);
+
+   wire  [1:0] encoder_o;
+
+   logic   [3:0] error_counter;
+   logic   [1:0] encoder_o_reg;
+   
+   logic       enable_decoder_in;
+   wire        valid_encoder_o;
+
+	//Logic for error for bit 0 and 1
+	logic [2:0] error_bit8;
+	logic [4:0] error_bit32;
+	int bad_bit_ct = 1;
+	int word_ct;
+	
+	
+   always @ (posedge clk, negedge rst) 
+      if(!rst) begin  
+         error_counter  <= 4'd0;
+         encoder_o_reg  <= 2'b00;
+         enable_decoder_in <= 1'b0;
+			error_bit8 <= 3'd0;
+			error_bit32 <= 5'd0;
+			 word_ct = 0;
+      end
+      else	   begin   
+         enable_decoder_in <= valid_encoder_o; 
+         encoder_o_reg  <= 2'b00;
+         error_counter  <= error_counter + 4'd1;
+			error_bit8 <= error_bit8 + 3'd1;
+			error_bit32 <= error_bit32 + 5'd1;
+			 word_ct              <= word_ct + 1;
+       
+		   if(error_bit32 == 5'b01010 || error_bit32 == 5'b01011 || error_bit32 == 5'b01100 || error_bit32 == 5'b01101 )begin //2a7 // # good = 240, bad =  16, Injected :32
+            encoder_o_reg  <= {~encoder_o[1],encoder_o[0]};	 // inject 4 in a row bad bit out of every 32 
+				 if(word_ct<256)begin
+					bad_bit_ct     <= bad_bit_ct + 1;
+					$display("error_counter = %d",bad_bit_ct);
+				 end
+			end
+			
+			else
+            encoder_o_reg  <= {encoder_o[1],encoder_o[0]};
+      end   
+
+// insert your convolutional encoder here
+// change port names and module name as necessary/desired
+   encoder encoder1	     (
+      .clk(clk),
+      .rst(rst),
+      .enable_i(enable_encoder_i),
+      .d_in(encoder_i),
+      .valid_o(valid_encoder_o),
+      .d_out(encoder_o)   );
+
+// insert your term project code here 
+   decoder decoder1	     (
+      .clk(clk),
+      .rst(rst),
+      .enable(enable_decoder_in),
+      .d_in(encoder_o_reg),
+      .d_out(decoder_o)
+		);
+
+endmodule
+
+
+
